3dca90f2fde65645638e3bf192d46b7c76fcdee7
Quoctuan1780 <Quoctuan1780@gmail.com>
2021-03-24 11:06:03 +0700

    Add template

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FinalProject.Controllers
 {
@@ -19,11 +15,6 @@ namespace FinalProject.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
